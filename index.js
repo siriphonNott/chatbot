129366366490751aa3d2919d061b94bbcf4ce6ca
siriphonnot <siriphonnot@gmail.com>
2019-01-21 15:26:25 +0700

    add condition

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 5000;
+const line = require('@line/bot-sdk');
 
-// parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
+const client = new line.Client({
+  channelAccessToken: 'X3lJ+f68IyxFIM0ZdbE39ZjN4cQBjQcpdo2JjmfFL14nR2xPwM+yhD4Pnm61q+3FN2e0XNez79ZHWvgLK+eqRjYCUqGRqjhQmOY+CQ/6Vv5ek/fyCIoPSCu3nwFkYNogCKSa7Vx0J1/uk4rPzL1SkAdB04t89/1O/w1cDnyilFU='
+});
  
 // parse application/json
 app.use(bodyParser.json())
@@ -15,16 +17,66 @@ app.get('/', (req, res) => {
 
 app.post('/webhook', (req, res) => {
   console.log('==> /webhook');
-  let body = req.body; 
-  let events = body.events[0];
-  let source = events.source;
-  let message = events.message;
+  let body = req.body || ''; 
   console.log('==> Body ');
   console.log(body);
-  console.log(`source ==>`);
-  console.log(source);
-  console.log(`message ==>`);
-  console.log(message);
+
+  if(req.body && body.events) {
+    let events = body.events[0];
+    let source = events.source;
+    let message = events.message;
+    let type = events.type;
+    let replyToken = events.replyToken;
+
+    console.log(`[type] ==>`);
+    console.log(type);
+    console.log(`[replyToken] ==>`);
+    console.log(replyToken);
+    console.log(`[source] ==>`);
+    console.log(source);
+    console.log(`[message] ==>`);
+    console.log(message);
+
+    switch (type) {
+      case 'message':
+        let type = message.type;
+        console.log(`[message type] ==> ${type}`);
+        let id = message.id;
+        if(type == 'text') {
+          let text = message.text;
+
+          const message = [
+            {
+              type: 'text',
+              text: `NottDev สวัสดีครับ มีอะไรหรอครับ? ก็มาดิครับ`
+            },
+            {
+              type: "sticker",
+              packageId: "51626496",
+              stickerId: "11538"
+            }
+          ];
+        
+        replyMessage(replyToken, message)
+
+        } else if(type == 'sticker') {
+          let stickerId = message.stickerId;
+          let packageId = message.packageId;
+        }
+        
+        break;
+      case 'follow':
+        break;
+      case 'unfollow':
+        break;
+      case 'join':
+        break;
+      case 'leave':
+        break;
+      default:
+        break;
+    }
+  }
 
   let response = {
     status: 'ok',
@@ -33,6 +85,22 @@ app.post('/webhook', (req, res) => {
   
   res.send(response);
 });
+
+//Method
+const replyMessage = (replyToken, message) => {
+  console.log('==> [replyMessage]');
+  console.log(`==> replyToken: ${replyToken}`);
+  console.log(`==> message: `);
+  console.log(message);
+
+  client.replyMessage(replyToken, message)
+  .then(() => {
+    console.log('replyMessage is successfully!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
 
 app.listen(port,() => {
   console.log(`Listening on port ${port}`);
